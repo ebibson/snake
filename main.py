254971183d13a34969b93131da005202fcc9718d
Raphael Learn <ebibende1@gmail.com>
2022-08-19 13:19:40 +0100

    create a snake and the fruit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os, sys
 import time  
+import random
 
 pygame.init()
 
@@ -8,8 +9,23 @@ SIZE = (WIDTH, HEIGHT) = (600, 600)
 
 # config the size and the title
 pygame.display.set_caption("Snake")
-pygame.display.set_mode(SIZE)
+screen = pygame.display.set_mode(SIZE)
 
+
+colors = ['Red']
+
+
+# create fruits the ywil be generated randomly
+fruit = pygame.Surface((10, 10))
+fruit.fill('Red')
+
+screen.blit(fruit, (random.randint(1, 599), random.randint(1, 599)))
+
+# create the Snake
+snake = pygame.Surface((20, 20))
+snake.fill('Green')
+
+screen.blit(snake, (200, 100))
 
 while 1:
     
@@ -17,7 +33,8 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            
 
-    #update every time
+   #update every time
     pygame.display.update()
 
